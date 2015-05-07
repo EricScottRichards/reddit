ae018dd069eab817d522aed379860c7b50df3708
EricScottRichards <ericscottrichards@gmail.com>
2015-05-06 20:06:04 -0600

    Completed through README line 99. Only Black Diamond left.

--- controller.js
+++ controller.js
@@ -1,6 +1,25 @@
 var app = angular.module('reddit');
-.controller('PostsController', function($scope, FirebaseService){
+app.controller('PostsController', function($scope, FirebaseService){
+
+	
+
+	var getPosts = function(){
+		FirebaseService.getRequest()
+			.then(function(data){
+				$scope.posts = data;
+			})
+	}
+
+	$scope.addPost = function(){
+		FirebaseService.addPost($scope.newPost)
+			.then(function(){
+				getPosts()
+			})
+	}
 
 
 
-}
+	getPosts();
+	
+
+});
